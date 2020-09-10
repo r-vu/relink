4f636b55d36e70af20e9bfc08d2e1773fa3198be
r-vu <46875289+r-vu@users.noreply.github.com>
2020-09-09 18:02:50 -0700

    Modify security policy and user detail service
    - Make previously created class JpaUserDetailsService as the default
      service for the application's AuthenicationManagerBuilder.
    
    - Add Configuration annotation to class definition in order to support
      the Autowired JpaUserDetailsService field.
    
    - Modify security policy by only permitting the home page to be used by
      all users. This means /table will be locked and only available to
      users who have logged in. If the user has not logged in and attempts
      to access /table, they will be redirected to /login. Currently there
      is no logout button yet.
    
    - As per the previous point, all pages except the home page ("/") were
      locked away. This also includes things such as JavaScript files and
      CSS files. Therefore the security policy was changed to explicitly
      give everybody permission to access said files in order to properly
      render other, permitted pages.

--- src/main/java/rvu/application/relink/WebSecurityConfig.java
+++ src/main/java/rvu/application/relink/WebSecurityConfig.java
@@ -1,16 +1,43 @@
 package rvu.application.relink;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JpaUserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.userDetailsService)
+            .passwordEncoder(RelinkUser.PASSWORD_HASHER);
+    }
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/*").permitAll();
+        http
+            .authorizeRequests()
+                .antMatchers(
+                    "/",
+                    "/built/**",
+                    "/*.css"
+                ).permitAll()
+                .anyRequest().authenticated()
+            .and().formLogin()
+                .permitAll()
+            .and().logout()
+                .permitAll();
         http.csrf(csrf -> csrf.disable());
     }
 
