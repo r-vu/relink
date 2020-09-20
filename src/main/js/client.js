e114cbff730d3df77911ca420471b3f47356e69e
r-vu <46875289+r-vu@users.noreply.github.com>
2020-09-20 16:08:20 -0700

    Add CSRF tokens to rest.js client

--- src/main/js/client.js
+++ src/main/js/client.js
@@ -7,17 +7,22 @@ const uriTemplateInterceptor = require("./api/uriTemplateInterceptor");
 const errorCode = require("rest/interceptor/errorCode");
 const baseRegistry = require("rest/mime/registry");
 const template = require("rest/interceptor/template");
-const pathPrefix = require("rest/interceptor/pathPrefix")
+const pathPrefix = require("rest/interceptor/pathPrefix");
+const csrf = require("rest/interceptor/csrf");
 
 const registry = baseRegistry.child();
 
 registry.register("text/uri-list", require("./api/uriListConverter"));
 registry.register("application/hal+json", require("rest/mime/type/application/hal"));
 
+let csrfToken = document.getElementsByTagName("meta")["_csrf"].getAttribute("value");
+let csrfHeader = document.getElementsByTagName("meta")["_csrf_header"].getAttribute("value");
+
 module.exports = rest
-        .wrap(pathPrefix, {prefix: "/api"})
-        .wrap(template, {template: "shortURLs{?size}", size: 20})
-        .wrap(mime, { registry: registry })
-        // .wrap(uriTemplateInterceptor)
-        .wrap(errorCode)
-        .wrap(defaultRequest, { headers: { "Accept": "application/hal+json" }});
+    .wrap(csrf, {token: csrfToken, name: csrfHeader})
+    .wrap(pathPrefix, {prefix: "/api"})
+    .wrap(template, {template: "shortURLs{?size}", size: 20})
+    .wrap(mime, { registry: registry })
+    // .wrap(uriTemplateInterceptor)
+    .wrap(errorCode)
+    .wrap(defaultRequest, { headers: { "Accept": "application/hal+json" }});
