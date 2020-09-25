bdf520d8f31edf09a9054a8e818302088f41208c
r-vu <46875289+r-vu@users.noreply.github.com>
2020-09-24 17:44:53 -0700

    Add owner only views for ShortURLs

--- src/main/java/rvu/application/relink/ShortURLRepository.java
+++ src/main/java/rvu/application/relink/ShortURLRepository.java
@@ -1,10 +1,21 @@
 package rvu.application.relink;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 public interface ShortURLRepository extends JpaRepository<ShortURL, Long> {
+
+    @Override
+    @Query("select s from ShortURL s where s.owner.name = ?#{authentication?.name}")
+    Page<ShortURL> findAll(Pageable pageable);
+
 
     ShortURL findByName(String name);
 
